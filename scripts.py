6ca98caf5519825d2675c105b07da050c21b1459
ツAʙʜɪsʜᴇᴋ Kᴜᴍᴀʀ <93422036+MrAbhi2k3@users.noreply.github.com>
2023-01-20 14:07:32 +0530

    Update scripts.py

--- scripts.py
+++ scripts.py
@@ -34,13 +34,13 @@ Cᴏᴍᴍᴀɴᴅs -
     ABOUT_TEXT = """
 ╭────[ Aʙᴏᴜᴛ Uꜱ ]────⍟
 │
-├ <b>Bᴏᴛ Nᴀᴍᴇ : <a href='https://t.me/AIOM_RENAMER_PRO_BOT'>Rᴇɴᴀᴍᴇʀ Bᴏᴛ</a></b>
+├ <b>Bᴏᴛ Nᴀᴍᴇ : <a href='https://t.me/+t1ko_FOJxhFiOThl'>Rᴇɴᴀᴍᴇʀ Bᴏᴛ</a></b>
 │
-├ <b>Cʜᴀɴɴᴇʟ : <a href='https://t.me/aiom_bots'>Aɪ๏ᴍ Bᴏᴛs</a></b>
+├ <b>Cʜᴀɴɴᴇʟ : <a href='https://t.me/TeleRoidGroup'>TeleRoidGroup</a></b>
 │
-├ <b>Vᴇʀsɪᴏɴ : <a href='https://t.me/AIOM_RENAMER_PRO_BOT'>2.9.2 Bᴇᴛᴀ</a></b>
+├ <b>Vᴇʀsɪᴏɴ : <a href='https://t.me/TeleRoid_RENAMER_BOT'>2.9.2 Bᴇᴛᴀ</a></b>
 │
-├ <b>Sᴏᴜʀᴄᴇ : <a href='https://t.me/AIOM_RENAMER_PRO_BOT'>Cᴏᴍɪɴɢ Sᴏᴏɴ</a></b>
+├ <b>Sᴏᴜʀᴄᴇ : <a href='https://t.me/SinglemachiVision'>Cᴏᴍɪɴɢ Sᴏᴏɴ</a></b>
 │
 ├ <b>Sᴇʀᴠᴇʀ : <a href='https://heroku.com'>Hᴇʀᴏᴋᴜ</a></b>
 │
@@ -48,9 +48,9 @@ Cᴏᴍᴍᴀɴᴅs -
 │
 ├ <b>Lᴀɴɢᴜᴀɢᴇ : <a href='https://www.python.org'>Pʏᴛʜᴏɴ 3.9.4</a></b>
 │
-├ <b>Dᴇᴠᴇʟᴏᴘᴇʀ : <a href='https://t.me/ajvadntr2'>محمد اجود</a></b>
+├ <b>Dᴇᴠᴇʟᴏᴘᴇʀ : <a href='https://t.me/HelpLessBoi'>Pʀᴇᴅ∆ᴛᴏʀ</a></b>
 │
-├ <b>Pᴏᴡᴇʀᴇᴅ Bʏ : <a href='https://t.me/aiom_bots'>Aɪ๏ᴍ Bᴏᴛs</a></b>
+├ <b>Pᴏᴡᴇʀᴇᴅ Bʏ : <a href='https://t.me/MoviesFlixers_DL'>MoviesFlixers</a></b>
 │
 ╰──────[ Tʜᴀɴᴋ Yᴏᴜ ]───⍟"""
 
@@ -66,15 +66,15 @@ Cᴏᴍᴍᴀɴᴅs -
     UPLOAD_SUCCESS = "<u><i>Tʜᴀɴᴋs Fᴏʀ Usɪɴɢ ᴍᴇ❤</i></u>"
     REPLY_TO_MEDIA = "<i>Reply to Media For Converting with Command /convert</i>"
     UPLOAD_START = "<b>UPLOADING</b>"
-    DOWNLOAD_START = "<b>RENAMEING</b>"
+    DOWNLOAD_START = "<b>RENAMING</b>"
     JOIN_NOW_TEXT = "Jᴏɪɴ Mʏ Uᴘᴅᴀᴛᴇ Cʜᴀɴɴᴇʟ Tᴏ Usᴇ Tʜɪs Bᴏᴛ</b>"
     REPLY_TO_FILE = "<b>Reply to that media with /rename new name .ext</i>"
-    CONTACT_MY_DEVELOPER = "<i>Something Went Wrong! Contact in Support @AIOM_BOTS</i>"
+    CONTACT_MY_DEVELOPER = "<i>Something Went Wrong! Contact in Support @TeleRoid14</i>"
     START_TEXT = """<b>Hᴇʟʟᴏ</b> 👋 <b>,  {} ♡
 
 Tʜɪꜱ Iꜱ A Fᴀꜱᴛᴇꜱᴛ Fɪʟᴇ Rᴇɴᴀᴍᴇ Bᴏᴛ Wɪᴛʜ Fɪʟᴇ Cᴏɴᴠᴇʀᴛᴇʀ Fᴜᴛᴜʀᴇ & Pᴇʀᴍᴀɴᴇɴᴛ Tʜᴜᴍʙɴᴀɪʟ Sᴜᴘᴘᴏʀᴛ
 
 Pʀᴇꜱꜱ Hᴇʟᴩ Bᴜᴛᴛᴏɴ Fᴏʀ Mᴏʀᴇ Iɴꜰᴏ...
 
-Pᴏᴡᴇʀᴇᴅ Bʏ : @AIOM_BOTS</b>"""
-    UPGRADE_TEXT = "<b>To upgrade your subscription <a href='https://t.me/ajvadntr'>[Click Here]</a></b>"
+Pᴏᴡᴇʀᴇᴅ Bʏ : @OWNYOURBOTZ</b>"""
+    UPGRADE_TEXT = "<b>To upgrade your subscription <a href='https://t.me/HelpLessBoi'>[Click Here]</a></b>"
