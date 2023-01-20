2a987816a71f89b7e83d6804a90b5cd36bee3b03
ツAʙʜɪsʜᴇᴋ Kᴜᴍᴀʀ <93422036+MrAbhi2k3@users.noreply.github.com>
2023-01-20 16:25:37 +0530

    Update cmds.py

--- plugins/cmds.py
+++ plugins/cmds.py
@@ -25,9 +25,7 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 if bool(os.environ.get("WEBHOOK", False)):
 
-    from sample_config import Config
-else:
-    from config import Config
+from sample_config import Config
 
 from PIL import Image
 from database.database import *
